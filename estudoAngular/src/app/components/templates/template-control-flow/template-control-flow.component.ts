4c29956537244cbbabef7cc99ee063f40cd1eedc
Henrique <78829197+henrique1023@users.noreply.github.com>
2024-07-10 16:19:37 -0300

    feat - adiciona control flow

--- estudoAngular/src/app/components/templates/template-control-flow/template-control-flow.component.ts
+++ estudoAngular/src/app/components/templates/template-control-flow/template-control-flow.component.ts
@@ -1,13 +1,32 @@
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { delay, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-template-control-flow',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AsyncPipe],
   templateUrl: './template-control-flow.component.html',
   styleUrl: './template-control-flow.component.scss'
 })
 export class TemplateControlFlowComponent {
   public isTrue = true;
+
+
+  public itens: Array<{name: string}> = [];
+
+  public condition: string = 'B';
+  public loadingData$: Observable<String[]> = of([
+    'Loading1...',
+    'Loading2...',
+    'Loading3...'
+  ]).pipe(delay(2000));
+
+  public trackByFn(index: number){
+    return index;
+  }
+
+  public addName(name:string){
+    return this.itens.push({name});
+  }
 }
